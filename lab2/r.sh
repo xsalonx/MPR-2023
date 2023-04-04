fe6c052c2ba2931556d1a9b6705bfebd7252ac45
dubiel <dubiel@dc1a-lab-vnode-01.ipa.ki.agh.edu.pl>
2023-04-04 11:46:44 +0200

    div ~ to num_threads added

--- lab2/r.sh
+++ lab2/r.sh
@@ -1,30 +1,58 @@
 #!/bin/bash
 
-# export LC_ALL=C
-
 BUILD_DIR="build"
 mkdir -p $BUILD_DIR
 PROG_NAME="prog.bin"
 
-SUBMEASURES_NO=${1:-10}
 export OMP_NUM_THREADS=${OMP_NUM_THREADS:-4}
+export OMP_SCHEDULE='NONE';
 
 MAX_NSIZE='1000000' #00'
 START_SIZE='100000'
 STEP='100000'
 
-rm build/*.csv
 
+
+
+while [[ $# -gt 0 ]]; do
+    case $1 in 
+        --num_threads_div_proportional)
+            lOMP_DIV_MULTIPLIER=$OMP_NUM_THREADS
+            OUT_FILE_SUFIX='-num-thread-div-proportional'
+            shift;
+        ;;
+        --sub_m)
+            lOMP_SUBMEASURES_NO=$2
+            shift 2;
+        ;;
+        --out)
+            OUT_FILE_BASE_NAME=$2
+            shift 2;
+        ;;
+        *)
+            echo "incorrect arg: $1"
+            exit 254
+        ;;
+    esac
+done
+
+export lOMP_DIV_MULTIPLIER=${lOMP_DIV_MULTIPLIER:-1}
+export lOMP_SUBMEASURES_NO=${lOMP_SUBMEASURES_NO:-10}
+OUT_FILE_BASE_NAME=${OUT_FILE_BASE_NAME:-openmp-measures}$OUT_FILE_SUFIX
+
+# rm build/*.csv
 g++ -Wall main.c -o $BUILD_DIR/$PROG_NAME -fopenmp
 
 
 
 
 cd $BUILD_DIR
-env | grep OMP_ > meta-measures.env
+env_file=meta-measures$OUT_FILE_SUFIX.env
+env | grep OMP_ > $env_file
+cat $env_file
 
 get_sch_fn() {
-    echo "openmp-measures_$*.csv"
+    echo "$OUT_FILE_BASE_NAME""_$*.csv"
 }
 
 
@@ -37,34 +65,55 @@ mk_meas() {
     fi
 
     echo -n "$s" >> $out
-    for i in $(seq $SUBMEASURES_NO); do
+    for i in $(seq $lOMP_SUBMEASURES_NO); do
         N_TIME=$(./$PROG_NAME $PARALLEL $s)
         echo -n ";$N_TIME" >> $out
     done
+    echo -n ";$sch_t" >> $out
     echo -n ";$SCH_T" >> $out
+    echo -n ";$chunk_size" >> $out
     echo ";$CHUNK_SIZE_DIV" >> $out
 }
 
 out=$(get_sch_fn)
+rm -f $out
 echo -n 'arr_size' >> "$out"
-for i in $(seq 1 $SUBMEASURES_NO); do echo -n ";at$i" >> "$out"; done
+for i in $(seq 1 $lOMP_SUBMEASURES_NO); do echo -n ";at$i" >> "$out"; done
 echo -n ";schedule_type" >> "$out";
+echo -n ";OMP_SCHEDULE" >> "$out";
+echo -n ";chunk_size" >> "$out";
 echo -n ";chunk_size_div" >> "$out";
 echo "" >> "$out"
 
 for (( s=$START_SIZE; s <= $MAX_NSIZE; s+=$STEP )); do
 # sequential
-    export OMP_SCHEDULE='seq'
+    chunk_size='0'
+    CHUNK_SIZE_DIV='0'
+    OMP_SCHEDULE='seq'
+    sch_t='seq'
     mk_meas 0
 # parallel auto
-    export OMP_SCHEDULE='auto'
+    chunk_size='0'
+    CHUNK_SIZE_DIV='0'
+    OMP_SCHEDULE='auto'
+    sch_t='auto'
     mk_meas 1
 # parallel with chunks
+
+    # chunks size = 1
     for sch_t in 'static' 'dynamic' 'guided'; do
-        for div in 1 $(seq 2 2 8); do
-            chunk_size=$(($s / $OMP_NUM_THREADS / $div))
-            export OMP_SCHEDULE="$sch_t,$chunk_size"
-            export CHUNK_SIZE_DIV=$(($OMP_NUM_THREADS * $div))
+        chunk_size=1
+        OMP_SCHEDULE="$sch_t,$chunk_size"
+        CHUNK_SIZE_DIV=0
+        mk_meas 1
+    done
+
+    # chunk_size = s / num_threads / div 
+    for sch_t in 'static' 'dynamic' 'guided'; do
+        for div in 1 $(seq 2 1 3); do
+            chunk_size=$(($s / $lOMP_DIV_MULTIPLIER / $div))
+            OMP_SCHEDULE="$sch_t,$chunk_size"
+            CHUNK_SIZE_DIV=$(($lOMP_DIV_MULTIPLIER * $div))
             mk_meas 1
         done
     done
