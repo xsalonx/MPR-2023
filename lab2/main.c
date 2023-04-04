c9f8dda1526d981b6be283af9b2121a1c19f9552
dubiel <dubiel@dc1a-lab-vnode-01.ipa.ki.agh.edu.pl>
2023-04-04 09:24:05 +0200

    measure script done

--- lab2/main.c
+++ lab2/main.c
@@ -3,8 +3,7 @@
 #include <omp.h>
 #include <time.h>
 
-// klauzula schedule, chunk 5 ustawień
-
+// 
 int* allocate_random_array(int parallel, size_t n, double min, double max) {
     int* ptr = (int*)malloc(sizeof(int) * n);
     int *pp;
@@ -15,7 +14,7 @@ int* allocate_random_array(int parallel, size_t n, double min, double max) {
         #pragma omp parallel shared(ptr), private(seed, pp)
         {
             pp = ptr;
-            #pragma omp for nowait, schedule(runtime)
+            #pragma omp for schedule(runtime)
             for (size_t i=0; i < n; i++) {
                 pp[i] =  min + erand48(seed);
             }
@@ -28,34 +27,14 @@ int* allocate_random_array(int parallel, size_t n, double min, double max) {
     }
 
     double t2 = omp_get_wtime();
-    printf("%f\n", (t2 - t1));
+    printf("%lf\n", (t2 - t1));
     return ptr;
 }
 
-// void hello_world() {
-//     int var1, var2, var3;
-//     printf("Threads no: %d\n", omp_get_num_threads());
-//     printf("My number is %d\n\n", omp_get_thread_num());
-
-//     #pragma omp parallel private (var1, var2) shared (var3)
-//     {
-//         printf("--Threads no: %d\n", omp_get_num_threads()) ;
-//         printf("--My number is %d\n", omp_get_thread_num()) ;
-//     }
-//     printf("\n");
-//     printf("Threads no: %d\n", omp_get_num_threads());
-//     printf("My number is %d\n\n", omp_get_thread_num());
-// }
 
 int main(int argc, char **argv) {
-    // hello_world();
-    size_t arr_size = strtoull(argv[1], NULL, 10);
-    int parallel;
-    // if (argc > 2) {
-        parallel = atoi(argv[2]);
-    // } else {
-    //     parallel = 0
-    // }
+    int parallel = atoi(argv[1]);
+    size_t arr_size = strtoull(argv[2], NULL, 10);
     int* ptr = allocate_random_array(parallel, arr_size, 0, 10);
     free(ptr);
     return 0;
--- lab2/r.sh
+++ lab2/r.sh
@@ -6,33 +6,68 @@ BUILD_DIR="build"
 mkdir -p $BUILD_DIR
 PROG_NAME="prog.bin"
 
-g++ -Wall main.c -o $BUILD_DIR/$PROG_NAME -fopenmp
-
 SUBMEASURES_NO=${1:-10}
-OUTFILE="openmp-measures.csv"
+export OMP_NUM_THREADS=${OMP_NUM_THREADS:-4}
 
 MAX_NSIZE='1000000' #00'
 START_SIZE='100000'
 STEP='100000'
 
+rm build/*.csv
+
+g++ -Wall main.c -o $BUILD_DIR/$PROG_NAME -fopenmp
+
+
+
+
 cd $BUILD_DIR
-echo -n "size"
-for at in $(seq $SUBMEASURES_NO); do echo -n ";at$at"; done
-echo
-for (( s=$START_SIZE; s <= $MAX_NSIZE; s+=$STEP)); do
-    echo -n "$s"
+env | grep OMP_ > meta-measures.env
+
+get_sch_fn() {
+    echo "openmp-measures_$*.csv"
+}
+
+
+mk_meas() {
+    PARALLEL=$1
+    out=$(get_sch_fn) # $OMP_SCHEDULE)
+    SCH_T=$OMP_SCHEDULE
+    if [[ $OMP_SCHEDULE == 'seq' ]]; then
+        OMP_SCHEDULE="auto"
+    fi
+
+    echo -n "$s" >> $out
     for i in $(seq $SUBMEASURES_NO); do
-        N_TIME=$(./$PROG_NAME 0 $MAX_NSIZE)
-        P_TIME=$(./$PROG_NAME 1 $MAX_NSIZE)
-        # echo -n ";$N_TIME|$P_TIME"
-        R=$(echo "scale=2; $N_TIME / $P_TIME "| bc -l)
-        echo -n "; $R"
+        N_TIME=$(./$PROG_NAME $PARALLEL $s)
+        echo -n ";$N_TIME" >> $out
     done
-    echo ""
+    echo -n ";$SCH_T" >> $out
+    echo ";$CHUNK_SIZE_DIV" >> $out
+}
+
+out=$(get_sch_fn)
+echo -n 'arr_size' >> "$out"
+for i in $(seq 1 $SUBMEASURES_NO); do echo -n ";at$i" >> "$out"; done
+echo -n ";schedule_type" >> "$out";
+echo -n ";chunk_size_div" >> "$out";
+echo "" >> "$out"
+
+for (( s=$START_SIZE; s <= $MAX_NSIZE; s+=$STEP )); do
+# sequential
+    export OMP_SCHEDULE='seq'
+    mk_meas 0
+# parallel auto
+    export OMP_SCHEDULE='auto'
+    mk_meas 1
+# parallel with chunks
+    for sch_t in 'static' 'dynamic' 'guided'; do
+        for div in 1 $(seq 2 2 8); do
+            chunk_size=$(($s / $OMP_NUM_THREADS / $div))
+            export OMP_SCHEDULE="$sch_t,$chunk_size"
+            export CHUNK_SIZE_DIV=$(($OMP_NUM_THREADS * $div))
+            mk_meas 1
+        done
+    done
 done
 
 cd -
-# echo N=$N_TIME
-# echo P=$P_TIME
-# R=$(echo "scale=20; $N_TIME / $P_TIME"| bc -l)
-# echo R=$R
