b0c39ba26190f768ef8b45cef2fc62fc157aa779
xsalonx <lukasz.dubiel3@gmail.com>
2023-04-25 13:49:40 +0200

    a

--- lab2/m-bucket.sh
+++ lab2/m-bucket.sh
@@ -5,6 +5,7 @@ echo "" > $OUT_FILENAME
 
 for i in $(seq 100); do
     bn=$((100 * $i))
+    ./bucket.sh 0 1000000 $bn >> $OUT_FILENAME
     ./bucket.sh 2 1000000 $bn >> $OUT_FILENAME
     ./bucket.sh 3 1000000 $bn >> $OUT_FILENAME
 done;
