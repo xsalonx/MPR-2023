3d3914feb79b38b45238a423fd0d9453e558827c
xsalonx <lukasz.dubiel3@gmail.com>
2023-04-25 12:02:57 +0200

    a

--- lab2/seq.sh
+++ lab2/seq.sh
@@ -3,8 +3,8 @@
 OUT_FILENAME=seq-res.txt
 echo "" > $OUT_FILENAME
 
-for i in $(seq 100); do
-    bn=$((1000 * $i))
+for i in $(seq 2000); do
+    bn=$((100 * $i))
     ./bucket.sh 0 10000000 $bn >> $OUT_FILENAME
 done;
 
